15e2e307fc1b864d4f63cdf9a1297a14cf245ac2
jamesevag <4jzDRbH2>
2020-05-10 19:58:49 +0200

    format

--- src/main/java/suite/TestCaseClass.java
+++ src/main/java/suite/TestCaseClass.java
@@ -9,25 +9,26 @@ import java.util.Hashtable;
 public class TestCaseClass extends HelperClass {
 
     static Dictionary<String, String> locations = new Hashtable<String, String>();
-    final static String urlPrefix= "https://www.spitogatos.gr/search/results/residential/sale/r";
+    final static String urlPrefix = "https://www.spitogatos.gr/search/results/residential/sale/r";
 
     static {
-        locations.put("Athens","102/m102m?ref=homepageMapSearchSR");
-        locations.put("Mykonos","176/m3681m?ref=homepageMapSearchSR");
+        locations.put("Athens", "102/m102m?ref=homepageMapSearchSR");
+        locations.put("Mykonos", "176/m3681m?ref=homepageMapSearchSR");
 
     }
-    public TestCaseClass(){
+
+    public TestCaseClass() {
     }
 
-        @Test
-        public void openAthensUrl() {
-                System.out.println("Open Athens results");
-                driver.get(urlPrefix+locations.get("Athens"));
-        }
+    @Test
+    public void openAthensUrl() {
+        System.out.println("Open Athens results");
+        driver.get(urlPrefix + locations.get("Athens"));
+    }
 
     @Test
     public void openMykonosUrl() {
         System.out.println("Open Mykonos results");
-        driver.get(urlPrefix+locations.get("Mykonos"));
+        driver.get(urlPrefix + locations.get("Mykonos"));
     }
 }
